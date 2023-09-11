c0eae3fdcf3c6b5c9db58b189d9f1c2371312750
Pedro Silva <phms2307@gmail.com>
2023-09-11 20:53:37 -0300

    ap03(selecao): import e estrutura

---
+++ ExemploIf.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+public class ExemploIf{
+   static public void main(String [] args) {
+    //declaração de variáveis
+    
+    //entrada
+
+    //processamento
+
+    //saída
+   }
+}
